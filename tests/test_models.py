3a799e06366318d576c030b484ca07def413a30f
wassimsalam01 <wassimsalam49@gmail.com>
2025-07-09 11:33:02 +0200

    parametrize daily_max unit tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -16,27 +16,15 @@ def test_daily_mean(test, expected):
     """Test mean function works for array of zeroes and positive integers."""
     npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
-def test_daily_max_zeros():
-    """Test that max function works for an array of zeros."""
-
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
-
-def test_daily_max_integers():
-    """Test that max function works for an array of positive integers."""
-
-    test_input = np.array([[5, 4],
-                           [8, 5],
-                           [1, 3]])
-    test_result = np.array([8, 5])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [5, 6]),
+    ])
+def test_daily_max(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
 def test_daily_min_zeros():
     """Test that min function works for an array of zeros."""
