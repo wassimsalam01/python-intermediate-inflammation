87f7d43e8ad4342b3bbb38cd78719a5ee6fc487e
wassimsalam01 <wassimsalam49@gmail.com>
2025-07-09 11:35:00 +0200

    parametrize daily_min unit tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -26,30 +26,12 @@ def test_daily_max(test, expected):
     """Test mean function works for array of zeroes and positive integers."""
     npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
-def test_daily_min_zeros():
-    """Test that min function works for an array of zeros."""
-
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_min(test_input), test_result)
-
-def test_daily_min_integers():
-    """Test that min function works for an array of positive integers."""
-
-    test_input = np.array([[7, 6],
-                           [1, 3],
-                           [5, 9]])
-    test_result = np.array([1, 3])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_min(test_input), test_result)
-
-def test_daily_min_string():
-    """Test for TypeError when passing strings"""
-
-    with pytest.raises(TypeError):
-        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [1, 2]),
+    ])
+def test_daily_min(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
