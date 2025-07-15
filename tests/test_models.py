8f0f3aa225446c45bb1cc5d5c6fa587c4ed204e1
Marten Jäger <martenjaeger@gmx.de>
2025-07-15 12:11:34 +0200

    last commit

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,6 +5,8 @@ import numpy.testing as npt
 import pytest
 
 from inflammation.models import daily_mean, daily_max, daily_min
+from inflammation.models import patient_normalise
+
 
 
 def test_daily_mean_zeros():
@@ -95,3 +97,16 @@ def test_daily_min_string():
 def test_daily_mean(test, expected):
     """Test mean function works for array of zeroes and positive integers."""
     npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+    ])
+def test_patient_normalise(test, expected):
+    """Test normalisation works for arrays of one and positive integers.
+       Test with a relative and absolute tolerance of 0.01."""
+
+    result = patient_normalise(np.array(test))
+    npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
