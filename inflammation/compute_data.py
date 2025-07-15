244458feb2fa42734dd88cfa5ffec866e00d9fe6
wassimsalam01 <wassimsalam49@gmail.com>
2025-07-15 13:14:43 +0200

    Decouple Data Loading from Data Analysis

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -7,18 +7,14 @@ import numpy as np
 
 from inflammation import models, views
 
-
 def analyse_data(data_dir):
     """Calculates the standard deviation by day between datasets.
 
     Gets all the inflammation data from CSV files within a directory,
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
 
+    data = models.load_inflammation_data(data_dir)
 
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
--- inflammation/models.py
+++ inflammation/models.py
@@ -7,8 +7,7 @@ inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
-import numpy as np
-
+import glob, numpy as np, os
 
 def load_csv(filename):  
     """Load a Numpy array from a CSV
@@ -17,6 +16,12 @@ def load_csv(filename):
     """
     return np.loadtxt(fname=filename, delimiter=',')
 
+def load_inflammation_data(data_dir):
+    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
+    if len(data_file_paths) == 0:
+        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
+    data = map(load_csv, data_file_paths)
+    return list(data)
 
 def daily_mean(data):
     """Calculate the daily mean of a 2d inflammation data array."""
